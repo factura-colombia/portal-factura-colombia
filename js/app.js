5fbd08b038c81cf1d0c4dce3d07daee18f332f84
jorge malagon <jorgemalagon.z@gmail.com>
2018-06-19 13:00:58 -0500

    Configuracion de sitios faltantes de Havas

--- js/app.js
+++ js/app.js
@@ -4,12 +4,15 @@
   app.controller("mainController", function ($scope, $http, $location) {
     function init() {
       var subdomain = window.location.host.split('.')[0];
+      $('body').addClass(subdomain);
       var url = '/config/' + subdomain + '-config.json';
       $http.get(url).success(function (response) {
         $scope.config = response;
-        
+
         var el = angular.element('.banner');
-        el.addClass(response.backgroundClass);
+        el.css({
+          'background-image': 'url(' + response.backgroundUrl + ')'
+        });
       });
     }
 
